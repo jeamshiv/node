8c10cd76c871f196b7abd91317b9879d7d698087
jeamshiv <58328919+jeamshiv@users.noreply.github.com>
2020-11-06 14:59:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res) =>{
+    res.sendFile(_dirname + "/index.html")
+})
+
+app.listen(PORT, () =>{
+    console.log(`App is running on port ${PORT}`)
+})
