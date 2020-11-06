21ca8c9ab371d4918ee4c9622a3bef7af5bd936b
jeamshiv <58328919+jeamshiv@users.noreply.github.com>
2020-11-06 15:45:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/',(req,res) =>{
-    res.sendFile(_dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(PORT, () =>{
