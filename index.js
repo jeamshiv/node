0c6f2e12fb63c754c5cb5761683090cdd20d248c
jeamshiv <58328919+jeamshiv@users.noreply.github.com>
2020-11-07 17:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+fs.mkdir('./assets', (err) =>{
+   if(err){
+       console.log(err);
+   }
+  console.log(`Folder has been created`);
+});
+
 app.get('/',(req,res) =>{
     res.sendFile(__dirname + "/index.html")
 })
